a381eaf85d0d714d9cf31870aae5c5533453eff5
NullVoxPopuli <LPSego3+dev@gmail.com>
2014-12-11 14:22:43 -0500

    updated travis and extconf

--- ext/kdiff3/extconf.rb
+++ ext/kdiff3/extconf.rb
@@ -1,3 +1,4 @@
+
 # Unconventional way of extconf-ing
 # http://yorickpeterse.com/articles/hacking-extconf-rb/
 require 'mkmf'
@@ -6,27 +7,20 @@ require 'mkmf'
 # $PATH.
 find_executable('make')
 find_executable('qmake')
+# create fake Makefile, courtesy of
+# https://github.com/leejarvis/ruby-c-example/tree/master/ext/hello
+# it's ugly, and a hack, but it continues the gem install process.
+# hello.c is small and meaningless in the context if this repository.
+create_makefile('hello')
 
-# Create a dummy extension file. Without this RubyGems would abort the
-# installation process. On Linux this would result in the file "wat.so"
-# being created in the current working directory.
-#
-# Normally the generated Makefile would take care of this but since we
-# don't generate one we'll have to do this manually.
-#
-# fake_extension = File.join(Dir.pwd, 'kdiff3.' + RbConfig::CONFIG['DLEXT'])
-# File.open(fake_extension, "w") {}
-
+# build kdiff3
+# - currently has to compile with qt4
+#   maybe someday, qt4 can be optional
 Dir.chdir('./') do
   exec './configure qt4'
 end
 
 # fail if compiling didn't succeed
 unless File.exist?('releaseQT/kdiff3')
-  abort('kdiff3 was not successfully compiled')
+  abort("\nERROR:  kdiff3 was not successfully compiled")
 end
-
-# This is normally set by calling create_makefile() but we don't need that
-# method since we'll provide a dummy Makefile. Without setting this value
-# RubyGems will abort the installation.
-$makefile_created = true
